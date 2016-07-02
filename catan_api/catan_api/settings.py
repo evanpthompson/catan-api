5e796a2ee8b53b22397c91a4dc204e75cf493b27
M. A. Owens <mattowens11@gmail.com>
2016-07-02 15:16:57 -0500

    Adds boards resource to app. Adds relationships. Adds create method

--- catan_api/catan_api/settings.py
+++ catan_api/catan_api/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'terrains',
     'paths',
-    'intersections'
+    'intersections',
+    'boards'
 )
 
 REST_FRAMEWORK = {
--- catan_api/intersections/serializers.py
+++ catan_api/intersections/serializers.py
@@ -2,7 +2,7 @@ from intersections.models import Intersection
 from rest_framework import serializers
 
 
-class PathSerializer(serializers.HyperlinkedModelSerializer):
+class IntersectionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Intersection
         fields = ('id')
